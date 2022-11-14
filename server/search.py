2e6aa2f7a4632477262edf454e775528f23da2fb
RK_PHG <3217009554@qq.com>
2022-11-14 11:07:21 +0800

    search.py

---
+++ server/search.py
@@ -0,0 +1,17 @@
+# 负责完成搜索逻辑
+
+def search(index,attr):
+
+#  实现单个属性值索引的搜索
+#  parma: index--该属性对应的索引
+#         attr--该属性的标签
+#  return：按顺序，返回该属性对应搜索结果的列表
+  pass
+
+def searchAll(index,attrs):
+
+#  实现多个属性值搜索的逻辑
+#  parma: index -- 该属性对应的索引
+#         attrs -- 属性的标签列表
+#         return -- 相关度排序后的结果
+  pass
